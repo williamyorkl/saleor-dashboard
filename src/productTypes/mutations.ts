4eda934a082d6c3beb31af0c7600b5c210483eb6
Dominik Żegleń <flesz3@o2.pl>
2022-02-15 16:23:25 +0100

    Use generated hooks in page types

--- src/productTypes/mutations.ts
+++ src/productTypes/mutations.ts
@@ -1,40 +1,4 @@
 import { gql } from "@apollo/client";
-import { productTypeDetailsFragment } from "@saleor/fragments/productTypes";
-import makeMutation from "@saleor/hooks/makeMutation";
-
-import { TypedMutation } from "../mutations";
-import {
-  AssignProductAttribute,
-  AssignProductAttributeVariables
-} from "./types/AssignProductAttribute";
-import {
-  ProductAttributeAssignmentUpdate,
-  ProductAttributeAssignmentUpdateVariables
-} from "./types/ProductAttributeAssignmentUpdate";
-import {
-  ProductTypeAttributeReorder,
-  ProductTypeAttributeReorderVariables
-} from "./types/ProductTypeAttributeReorder";
-import {
-  ProductTypeBulkDelete,
-  ProductTypeBulkDeleteVariables
-} from "./types/ProductTypeBulkDelete";
-import {
-  ProductTypeCreate,
-  ProductTypeCreateVariables
-} from "./types/ProductTypeCreate";
-import {
-  ProductTypeDelete,
-  ProductTypeDeleteVariables
-} from "./types/ProductTypeDelete";
-import {
-  ProductTypeUpdate,
-  ProductTypeUpdateVariables
-} from "./types/ProductTypeUpdate";
-import {
-  UnassignProductAttribute,
-  UnassignProductAttributeVariables
-} from "./types/UnassignProductAttribute";
 
 export const productTypeDeleteMutation = gql`
   mutation ProductTypeDelete($id: ID!) {
@@ -49,10 +13,6 @@ export const productTypeDeleteMutation = gql`
     }
   }
 `;
-export const TypedProductTypeDeleteMutation = TypedMutation<
-  ProductTypeDelete,
-  ProductTypeDeleteVariables
->(productTypeDeleteMutation);
 
 export const productTypeBulkDeleteMutation = gql`
   mutation ProductTypeBulkDelete($ids: [ID]!) {
@@ -64,13 +24,8 @@ export const productTypeBulkDeleteMutation = gql`
     }
   }
 `;
-export const TypedProductTypeBulkDeleteMutation = TypedMutation<
-  ProductTypeBulkDelete,
-  ProductTypeBulkDeleteVariables
->(productTypeBulkDeleteMutation);
 
 export const productTypeUpdateMutation = gql`
-  ${productTypeDetailsFragment}
   mutation ProductTypeUpdate($id: ID!, $input: ProductTypeInput!) {
     productTypeUpdate(id: $id, input: $input) {
       errors {
@@ -83,13 +38,8 @@ export const productTypeUpdateMutation = gql`
     }
   }
 `;
-export const useProductTypeUpdateMutation = makeMutation<
-  ProductTypeUpdate,
-  ProductTypeUpdateVariables
->(productTypeUpdateMutation);
 
 export const assignProductAttributeMutation = gql`
-  ${productTypeDetailsFragment}
   mutation AssignProductAttribute(
     $id: ID!
     $operations: [ProductAttributeAssignInput!]!
@@ -105,13 +55,8 @@ export const assignProductAttributeMutation = gql`
     }
   }
 `;
-export const TypedAssignProductAttributeMutation = TypedMutation<
-  AssignProductAttribute,
-  AssignProductAttributeVariables
->(assignProductAttributeMutation);
 
 export const unassignProductAttributeMutation = gql`
-  ${productTypeDetailsFragment}
   mutation UnassignProductAttribute($id: ID!, $ids: [ID]!) {
     productAttributeUnassign(productTypeId: $id, attributeIds: $ids) {
       errors {
@@ -124,13 +69,8 @@ export const unassignProductAttributeMutation = gql`
     }
   }
 `;
-export const TypedUnassignProductAttributeMutation = TypedMutation<
-  UnassignProductAttribute,
-  UnassignProductAttributeVariables
->(unassignProductAttributeMutation);
 
 export const productTypeCreateMutation = gql`
-  ${productTypeDetailsFragment}
   mutation ProductTypeCreate($input: ProductTypeInput!) {
     productTypeCreate(input: $input) {
       errors {
@@ -143,13 +83,8 @@ export const productTypeCreateMutation = gql`
     }
   }
 `;
-export const TypedProductTypeCreateMutation = TypedMutation<
-  ProductTypeCreate,
-  ProductTypeCreateVariables
->(productTypeCreateMutation);
 
-const productTypeAttributeReorder = gql`
-  ${productTypeDetailsFragment}
+export const productTypeAttributeReorder = gql`
   mutation ProductTypeAttributeReorder(
     $move: ReorderInput!
     $productTypeId: ID!
@@ -170,13 +105,8 @@ const productTypeAttributeReorder = gql`
     }
   }
 `;
-export const ProductTypeAttributeReorderMutation = TypedMutation<
-  ProductTypeAttributeReorder,
-  ProductTypeAttributeReorderVariables
->(productTypeAttributeReorder);
 
 export const productAttributeAssignmentUpdate = gql`
-  ${productTypeDetailsFragment}
   mutation ProductAttributeAssignmentUpdate(
     $operations: [ProductAttributeAssignmentUpdateInput]!
     $productTypeId: ID!
@@ -196,8 +126,3 @@ export const productAttributeAssignmentUpdate = gql`
     }
   }
 `;
-
-export const useProductAttributeAssignmentUpdateMutation = makeMutation<
-  ProductAttributeAssignmentUpdate,
-  ProductAttributeAssignmentUpdateVariables
->(productAttributeAssignmentUpdate);
